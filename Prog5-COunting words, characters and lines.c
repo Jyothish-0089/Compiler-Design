dc8050cf64faf79a4738f43a58acd69ab84e505c
Kaizer_Izumi <112233651+Jyothish-0089@users.noreply.github.com>
2023-08-08 20:50:08 +0530

    Update Prog5-COunting words, characters and lines.c

--- Prog5-COunting words, characters and lines.c
+++ Prog5-COunting words, characters and lines.c
@@ -1,26 +1,23 @@
-//5. Program to count number of words,lines and characters
+//5. Program to count the number of words , characters and lines.
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 int main(){
-	char str[100];
-	int i, words = 0 , newline = 0, characters = 0;
-	printf("Enter a Line of Text:");
-	scanf("%[^~]s",str);
-	for(i=0;str[i]!='\0';i++){
-		if(str[i]==' '){
+	int i, words = 0, characters = 0, lines = 0;
+	char input[100];
+	printf("Enter text line: ");
+	scanf("%[^~]s",input);
+	for(i=0;input[i]!='\0';i++){
+		if(input[i]==' '){
 			words++;
 		}
-		else if(str[i]=='\n'){
+		else if(input[i]=='\n'){
+			lines++;
 			words++;
-			newline++;
 		}
 		else{
 			characters++;
 		}
 	}
-	if(characters > 0){
-		words++;
-		newline++;
-	}
-	printf("%s%d%s%d%s%d","\nWords: ",words-1,"\nLines: ",newline-1,"\nCharacters: ",characters);
+	printf("%s%d%s%d%s%d","\nWords: ",words,"\nCharacters: ",characters,"\nLines: ",lines);
 }
