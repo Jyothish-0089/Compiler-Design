c963bf68d92e50e250d02f2e675a64730b56f208
Kaizer_Izumi <112233651+Jyothish-0089@users.noreply.github.com>
2023-08-09 09:47:38 +0530

    Update Prog3-Ignoring spaces, comments and newlines.c

--- Prog3-Ignoring spaces, comments and newlines.c
+++ Prog3-Ignoring spaces, comments and newlines.c
@@ -1,42 +1,49 @@
-//3. Program to ignore the redundant spaces, tabs, new lines and comments.
 #include<stdio.h>
-#include<conio.h>
+#include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
+ 
+int isKeyword(char buffer[]){
+	char keywords[32][10] = {"main","auto","break","case","char","const","continue","default",
+	"do","double","else","enum","extern","float","for","goto",
+	"if","int","long","register","return","short","signed",
+	"sizeof","static","struct","switch","typedef",
+	"unsigned","void","printf","while"};
+	int i, flag = 0;
+	for(i = 0; i < 32; ++i){
+		if(strcmp(keywords[i], buffer) == 0){
+			flag = 1;
+			break;
+		}
+	}
+	return flag;
+}
 int main(){
-	int i,j=0,flag=1,len;
-	char input[50],output[50];
-	printf("Enter Text Line: ");
-	scanf("%[^~]s",input);
-	len = strlen(input);
-	for(i=0;input[i]!='\0';i++){
-		if(input[i]=='/' && input[i+1]=='/'){
-			flag = 0;
-		}
-		if(input[i]=='\n'){
-			flag = 1;
-		}
-		if(input[i]=='/' && input[i+1]=='*'){
-			flag = 0;
-		}
-		if(input[i]=='*' && input[i+1]=='/'){
-			flag = 1;
-			i=i+2;
-		}
-		if(isspace(input[i])){
-			continue;
-		}
-		if(input[i]=='\n'){
-			continue;
-		}
-		else{
-			if(flag==1){
-				output[j]=input[i];
-			    j++;
-			}
-		}
+	char ch, buffer[15], operators[] = "+-*/%=";
+	FILE *fp;
+	int i,j=0;
+	fp = fopen("exp3input.txt","r");
+	if(fp == NULL){
+		printf("Error while opening the file\n");
+		exit(0);
 	}
-	for(i=0;i<j;i++){
-		printf("%c",output[i]);
+	while((ch = fgetc(fp)) != EOF){
+	   	for(i = 0; i < 6; ++i){
+	   		if(ch == operators[i])
+	  		printf("%c is operator\n", ch);
+	   	} 
+	   	if(isalnum(ch)){
+	   		buffer[j++] = ch;
+	   	}
+	   	else if((ch == ' ' || ch == '\n') && (j != 0)){
+	   		buffer[j] = '\0';
+	   		j = 0;
+	   		if(isKeyword(buffer) == 1)
+	   			printf("%s is Keyword\n", buffer);
+	   		else
+	   			printf("%s is Identifier\n", buffer);
+	    }
 	}
+	fclose(fp);
+	return 0;
 }
